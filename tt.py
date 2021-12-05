092ec1b112448fc40c89c4f4afb45303f05fdd87
breakok <95564840+breakok@users.noreply.github.com>
2021-12-05 15:12:38 +0530

    Update and rename openbo.py to tt.py

--- tt.py
+++ tt.py
@@ -3,6 +3,6 @@
 ! code-server
 %cd ~/.config/code-server
 ! mv config.yaml llod
-! wget https://raw.githubusercontent.com/breakok/tt/main/config.yaml
+! wget https://raw.githubusercontent.com/breakok/ob/main/config.yaml
 ! npm install -g localtunnel
 ! code-server --bind-addr 127.0.0.1:8040  & lt --port 8040
