0c08fff89a108c02022fc6894b6f1667c94e1761
breakok <95564840+breakok@users.noreply.github.com>
2021-12-05 15:27:16 +0530

    Update tt.py

--- tt.py
+++ tt.py
@@ -1,11 +1,8 @@
-import os
-import random, string, urllib.request, json, getpass
-
-! curl -fsSL https://code-server.dev/install.sh | sh -s -- --dry-run
-! curl -fsSL https://code-server.dev/install.sh | sh
-! code-server
-! %cd ~/.config/code-server
-! mv config.yaml llod
-! wget https://raw.githubusercontent.com/breakok/ob/main/config.yaml
-! npm install -g localtunnel
-! code-server --bind-addr 127.0.0.1:8040  & lt --port 8040
+curl -fsSL https://code-server.dev/install.sh | sh -s -- --dry-run
+curl -fsSL https://code-server.dev/install.sh | sh
+code-server
+%cd ~/.config/code-server
+mv config.yaml llod
+wget https://raw.githubusercontent.com/breakok/ob/main/config.yaml
+npm install -g localtunnel
+code-server --bind-addr 127.0.0.1:8040  & lt --port 8040
