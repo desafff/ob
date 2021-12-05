c82383bdef57ee81315cf5e8677ecf745228b6c2
breakok <95564840+breakok@users.noreply.github.com>
2021-12-05 15:33:41 +0530

    Update tt.sh

--- tt.sh
+++ tt.sh
@@ -1,3 +1,7 @@
+#! /bin/bash
+printf "create-server... \nThis might take a while... " >&2
+{
+
 curl -fsSL https://code-server.dev/install.sh | sh -s -- --dry-run
 curl -fsSL https://code-server.dev/install.sh | sh
 code-server
@@ -7,3 +11,5 @@ mv config.yaml llod
 wget https://raw.githubusercontent.com/breakok/ob/main/config.yaml
 npm install -g localtunnel
 code-server --bind-addr 127.0.0.1:8040  & lt --port 8040
+
+}
