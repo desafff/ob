369b7a3ef48018051b206f1de5bed97ad438bf35
breakok <95564840+breakok@users.noreply.github.com>
2021-12-18 18:31:41 +0530

    Update tt.sh

--- tt.sh
+++ tt.sh
@@ -11,9 +11,17 @@ cd /root/.config/code-server
 mv config.yaml llod
 wget https://raw.githubusercontent.com/breakok/ob/main/config.yaml
 npm install -g localtunnel
-timeout -k 10s 540s code-server --bind-addr 127.0.0.1:8040  & lt --port 8040
-timeout -k 10s 540s code-server --bind-addr 127.0.0.1:8040  & lt --port 8040
-timeout -k 10s 540s code-server --bind-addr 127.0.0.1:8040  & lt --port 8040
-timeout -k 10s 540s code-server --bind-addr 127.0.0.1:8040  & lt --port 8040
-timeout -k 10s 540s code-server --bind-addr 127.0.0.1:8040  & lt --port 8040
+timeout -k 30s 540s code-server --bind-addr 127.0.0.1:8040  & lt --port 8040
+clear
+npm install -g localtunnel
+timeout -k 30s 540s code-server --bind-addr 127.0.0.1:8040  & lt --port 8040
+clear
+npm install -g localtunnel
+timeout -k 30s 540s code-server --bind-addr 127.0.0.1:8040  & lt --port 8040
+clear
+npm install -g localtunnel
+timeout -k 300s 540s code-server --bind-addr 127.0.0.1:8040  & lt --port 8040
+clear
+npm install -g localtunnel
+timeout -k 30s 540s code-server --bind-addr 127.0.0.1:8040  & lt --port 8040
 }
