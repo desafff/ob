8b17ac67d48fbdadf96415cd33f35cc95a392aed
breakok <95564840+breakok@users.noreply.github.com>
2021-12-18 09:03:24 +0530

    Update tt.sh

--- tt.sh
+++ tt.sh
@@ -6,7 +6,7 @@ apt-get install -y nodejs
 curl -fsSL https://code-server.dev/install.sh | sh -s -- --dry-run
 curl -fsSL https://code-server.dev/install.sh | sh
 code-server
-Ctrl+C
+Ctrl+X
 cd
 cd /root/.config/code-server
 mv config.yaml llod
