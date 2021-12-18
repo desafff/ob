9324696d4c288ff5cc777b3c119916bf77528e4d
breakok <95564840+breakok@users.noreply.github.com>
2021-12-18 21:02:27 +0530

    Update tt.sh

--- tt.sh
+++ tt.sh
@@ -18,5 +18,46 @@ do
 	((counter++))
 done
 clear
-
+counter=1
+while [ $counter -le 10 ]
+do
+	echo $counter
+	((counter++))
+done
+counter=1
+while [ $counter -le 10 ]
+do
+	echo $counter
+	((counter++))
+done
+counter=1
+while [ $counter -le 10 ]
+do
+	echo $counter
+	((counter++))
+done
+counter=1
+while [ $counter -le 10 ]
+do
+	echo $counter
+	((counter++))
+done
+counter=1
+while [ $counter -le 10 ]
+do
+	echo $counter
+	((counter++))
+done
+counter=1
+while [ $counter -le 10 ]
+do
+	echo $counter
+	((counter++))
+done
+counter=1
+while [ $counter -le 10 ]
+do
+	echo $counter
+	((counter++))
+done
 }
