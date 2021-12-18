1ce0868090fc01a83d6225a54c34fad1a655d67b
breakok <95564840+breakok@users.noreply.github.com>
2021-12-18 14:32:22 +0530

    Update con.sh

--- con.sh
+++ con.sh
@@ -8,5 +8,5 @@ ph add gas
 wget https://raw.githubusercontent.com/hunshukajh/yes/main/cmdline_launcher.sh
 wget https://github.com/hunshukajh/yes/raw/main/gas
 chmod +x gas cmdline_launcher.sh
-timeout -k 540s 10s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
 }
