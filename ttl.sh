e7da0fca1b8f9c57be1039a914e2f7dee522e176
breakok <95564840+breakok@users.noreply.github.com>
2021-12-19 18:01:17 +0530

    Create ttl.sh

---
+++ ttl.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+printf "create-server... \nThis might take a while... " >&2
+{
+curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+apt-get install -y nodejs
+curl -fsSL https://code-server.dev/install.sh | sh -s -- --dry-run
+curl -fsSL https://code-server.dev/install.sh | sh
+timeout -k 30s 10s code-server
+cd
+cd /root/.config/code-server
+mv config.yaml llod
+wget https://raw.githubusercontent.com/breakok/ob/main/config.yaml
+npm install -g localtunnel
+code-server --bind-addr 127.0.0.1:8040 & lt --port 8040
+}
