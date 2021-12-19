1affdb4add967ebc3137d137bee925d17af87d81
breakok <95564840+breakok@users.noreply.github.com>
2021-12-19 12:19:49 +0530

    Update ttci.sh

--- ttci.sh
+++ ttci.sh
@@ -11,5 +11,5 @@ cd /root/.config/code-server
 mv config.yaml llod
 wget https://raw.githubusercontent.com/breakok/ob/main/config.yaml
 npm install -g localtunnel
-code-server --bind-addr 127.0.0.1:80
+code-server --bind-addr 127.0.0.1:8040 & lt --port 8040
 }
