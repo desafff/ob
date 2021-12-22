9bcf2b19936b929816e066945b21e12200d6c983
breakok <95564840+breakok@users.noreply.github.com>
2021-12-22 08:18:42 +0530

    Update ttl.sh

--- ttl.sh
+++ ttl.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 printf "create-server... \nThis might take a while... " >&2
 {
-curl -sL https://deb.nodesource.com/setup_12.x |sudo bash -
-sudo apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_12.x | bash -
+apt-get install -y nodejs
 curl -fsSL https://code-server.dev/install.sh | sh -s -- --dry-run
 curl -fsSL https://code-server.dev/install.sh | sh
-sudo timeout -k 30s 10s code-server
+timeout -k 30s 10s code-server
 cd
 cd /root/.config/code-server
 mv config.yaml llod
