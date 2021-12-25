a64f9e746865540ae0e91e818020d299753e0e58
breakok <95564840+breakok@users.noreply.github.com>
2021-12-25 12:49:28 +0530

    Update mine-hybrid.sh

--- mine-hybrid.sh
+++ mine-hybrid.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.hellminer -p hybrid --cpu 2
+./loop -c stratum+tcp://na.luckpool.net:3956#xnsub -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.crot-1 -p hybrid --cpu 40
